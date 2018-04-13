571496d9d6e8a2c5c5180dedb420cccbcc10767e
machamochi <37142785+machamochi@users.noreply.github.com>
2018-04-13 11:28:41 +0300

    Update treenit.php
    puuttui >

--- treenit.php
+++ treenit.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="w3-update.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="font-awesome_min.css"
+    <link rel="stylesheet" href="font-awesome_min.css">
     <body id="myPage">
 
         <!-- Sidebar on click -->
