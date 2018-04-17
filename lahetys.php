fbc081b3460479be80e974918bf96e00df84e01f
Wille Tuovinen <wille.tuovinen@metropolia.fi>
2018-04-17 12:29:05 +0300

    ..
    ...

--- lahetys.php
+++ lahetys.php
@@ -1,21 +1,15 @@
 <?php
-/*
-This first bit sets the email address that you want the form to be submitted to.
-You will need to change this value to a valid email address that you can access.
-*/
+// fixfitin mailiosoite
 $webmaster_email = "info.fixfit@gmail.com";
 
-/*
-This bit sets the URLs of the supporting pages.
-If you change the names of any of the pages, you will need to change the values here.
-*/
+// testpages for testing...
 $feedback_page = "yhteystiedot.php";
 $error_page = "error_message.html";
 $thankyou_page = "kiitos.php";
 
 /*
-This next bit loads the form field data into variables.
-If you add a form field, you will need to add it here.
+Seuraavassa annettu data muuttujiin.
+Form fieltdit tähän, jos haluaa sellaisen.
 */
 $email_address = $_POST["email"] ;
 $comments = $_POST["message"] ;
@@ -25,10 +19,7 @@ $msg =
 "Email: " . $email_address . "\r\n" . 
 "Comments: " . $comments ;
 
-/*
-The following function checks for email injection.
-Specifically, it checks for carriage returns - typically used by spammers to inject a CC list.
-*/
+// Seurava toiminto tarkastaaa maili-osoitteen syötön.
 function isInjected($str) {
 	$injections = array('(\n+)',
 	'(\r+)',
@@ -53,21 +44,17 @@ if (!isset($_POST['email'])) {
 header( "Location: $feedback_page" );
 }
 
-// If the form fields are empty, redirect to the error page.
+// jos formfield on tyhjä, tämä ohjaa errorpagelle.
 elseif (empty($first_name) || empty($email_address)) {
 header( "Location: $error_page" );
 }
 
-/* 
-If email injection is detected, redirect to the error page.
-If you add a form field, you should add it here.
-*/
-
+// Jos email ei ole oikein, tämä lähettää errorpagelle.
 elseif ( isInjected($email_address) || isInjected($first_name)  || isInjected($comments) ) {
 header( "Location: $error_page" );
 }
 
-// If we passed all previous tests, send the email, then redirect to the thank you page.
+// jos kaikki edellä oleet testit läpäistään, maili lähetetään ja ohjataan "kiitos!" sivulle.
 else {
 
 	mail( "$webmaster_email", "Feedback Form Results", $msg );
