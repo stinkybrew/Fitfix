22273baad53b6175e0adae50ac196609f2a038c4
machamochi <37142785+machamochi@users.noreply.github.com>
2018-04-20 17:15:45 +0300

    Update register2.php
    pieniä muutoksia lisää

--- register2.php
+++ register2.php
@@ -9,19 +9,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
     <body id="myPages" class="register">
-
-        <!-- Sidebar on click -->
-        <nav class="w3-sidebar w3-bar-block w3-white w3-card w3-animate-left w3-xxlarge" style="display:none;z-index:2" id="mySidebar">
-            <a href="javascript:void(0)" onclick="w3_close()" class="w3-bar-item w3-button w3-display-topright w3-text-teal">Close
-                <i class="fa fa-remove"></i>
-            </a>
-            <a href="#" class="w3-bar-item w3-button">Link 1</a>
-            <a href="#" class="w3-bar-item w3-button">Link 2</a>
-            <a href="#" class="w3-bar-item w3-button">Link 3</a>
-            <a href="#" class="w3-bar-item w3-button">Link 4</a>
-            <a href="#" class="w3-bar-item w3-button">Link 5</a>
-        </nav>
-
+        
         <!-- Navbar -->
         <div class="w3-top">
             <div class="w3-bar w3-theme-d2 w3-left-align">
@@ -32,10 +20,6 @@
 
             <!-- Navbar on small screens -->
             <div id="navDemo" class="w3-bar-block w3-theme-d2 w3-hide w3-hide-large w3-hide-medium">
-                <a href="#team" class="w3-bar-item w3-button">Team</a>
-                <a href="#work" class="w3-bar-item w3-button">Work</a>
-                <a href="#pricing" class="w3-bar-item w3-button">Price</a>
-                <a href="#contact" class="w3-bar-item w3-button">Contact</a>
                 <a href="#" class="w3-bar-item w3-button">Search</a>
             </div>
         </div>
@@ -46,7 +30,7 @@
                 <div class="w3-third w3-display-middle">
                     <h1 class="w3-center">Rekisteröidy tästä!</h1>
                     <form class="w3-container w3-card-4 w3-padding-16 w3-white" action="lahetys.php" method="post">
-                      <div class="w3-section">
+                        <div class="w3-section">
                             <label>Etunimi</label>
                             <input class="w3-input" type="text" name="name" required>
                         </div>
@@ -59,7 +43,7 @@
                             <input class="w3-input" type="date" name="dob" required>
                         </div>
                         <div class="w3-section">
-                            <label>Sähköposti (testi.esimerkki@gmail.com)</label>
+                            <label>Sähköpostiosoite</label>
                             <input class="w3-input" type="text" name="email" required>
                         </div>
                         <div class="w3-section">
@@ -67,22 +51,24 @@
                             <input class="w3-input" type="text" name="email" required>
                         </div>
                         <div class="w3-section">
-                            <label for="password">Salasana (min. 8 merkkiä, käytäthän isoja ja pieniä kirjaimia sekä numeroita ja/tai erikoismerkkejä)</label>
+                            <label for="password">Salasana (minimi 8 merkkiä)</label>
                             <input class="w3-input" type="password" name="password" required>
-                            <label for="psw-repeat"><b>Repeat Password</b></label>
+                        </div>                   
+                        <div class="w3-section">
+                            <label for="psw-repeat">Toista salasana</label>
                             <input type="password" name="psw-repeat" required>
                         </div>
-                        <button type="reset" style="display:inline" class="w3-button w3-xlarge w3-right w3-theme" value="Reset">Tyhjennä</button>
-                        <input type="submit" style="display:inline;margin-right:2px" class="w3-button w3-xlarge w3-right w3-theme" value="Lähetä">
+                        <button type="reset" style="display:inline" class="w3-button w3-large w3-right w3-theme" value="Reset">Tyhjennä</button>
+                        <input type="submit" style="display:inline;margin-right:2px" class="w3-button w3-large w3-right w3-theme" value="Lähetä">
 
-                        <button type="button" onclick="document.getElementById('id01').style.display='block'" class="w3-button w3-xlarge w3-theme" title="Kysymys">Kysymyksiä?</button>
+                        <button type="button" onclick="document.getElementById('id01').style.display='block'" class="w3-button w3-large w3-theme" title="Kysymys">Kysymyksiä?</button>
                     </form>
                 </div>
                 <!--modal-->
                 <div id="id01" class="w3-modal">
                     <div class="w3-modal-content w3-card-4 w3-animate-top">
                         <header class="w3-container w3-teal w3-display-container"> 
-                            <span onclick="document.getElementById('id01').style.display='none'" class="w3-button w3-teal w3-display-topright"><i class="fa fa-remove"></i></span>
+                            <span onclick="document.getElementById('id01').style.display='none'" class="w3-button w3-teal w3-large w3-display-topright"><i class="fa fa-remove"></i></span>
                             <h4>Kysymyksiä?</h4>
                         </header>
                         <div class="w3-container">
@@ -92,7 +78,7 @@
                                     <input class="w3-input" type="text" name="name">
                                 </div>
                                 <div class="w3-section">
-                                    <label>Sähköpostiosoite(testi.esimerkki@gmail.com)</label>
+                                    <label>Sähköpostiosoite (testi.esimerkki@gmail.com)</label>
                                     <input class="w3-input" type="text" name="email">
                                 </div>
                                 <div class="w3-section">
@@ -111,7 +97,6 @@
                 </div>
             </div>
         </div>
-
         <!-- Footer 
 <footer class="w3-container w3-padding-32 w3-theme-d1 w3-center w3-bottom w3-hide-small" id="footer">
 
