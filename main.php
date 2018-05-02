d4a516d8f53210d70fcfcc36393215c0e76dff2d
stinkybrew <wille.tuovinen@metropolia.fi>
2018-05-02 11:41:35 +0300

    ..
    ....

--- main.php
+++ main.php
@@ -179,7 +179,7 @@ fclose($testia);
             <img class="mySlides w3-animate-left" src="img/running_man_wider6.jpg" alt="runningman" style="width:100%;min-height:150px;max-height:600px;">
             <img class="mySlides w3-animate-left" src="img/running_man_wider7.jpg" alt="runningman" style="width:100%;min-height:150px;max-height:600px;">
             <div class="w3-container w3-display-bottomleft w3-margin-bottom">  
-                <button onclick="document.getElementById('id01').style.display='block'" class="w3-button w3-xlarge w3-theme w3-hover-teal" title="Go To W3.CSS">mainosbanneri testiä</button>
+                <h1 class="w3-xlarge w3-theme w3-hover-teal" style="padding:10px">Liiku ja kehity!</h1>
             </div>
         </div>
         <!-- Modal -->
