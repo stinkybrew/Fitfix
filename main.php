b6efd6aae195e9d3b51461872ee35657fee91f42
stinkybrew <wille.tuovinen@metropolia.fi>
2018-05-02 12:41:06 +0300

    Ei jostain syystä ollut tota oikeata hreffiä

--- main.php
+++ main.php
@@ -59,12 +59,12 @@ fclose($testia);
                     <div class="w3-dropdown-hover w3-hide-small">
                         <button class="w3-button" title="Notifications"><a href="treenit.php">Treenit</a><i class="fa fa-caret-down"></i></button>
                         <div class="w3-dropdown-content w3-card-4 w3-bar-block">
-                        <a href="#Käsitreenit" class="w3-bar-item w3-button">Kädet</a>
-                        <a href="#Jalkatreenit" class="w3-bar-item w3-button">Jalat</a>
-                        <a href="#Rintatreenit" class="w3-bar-item w3-button">Rinta</a>
-                        <a href="#Vatsatreenit" class="w3-bar-item w3-button">Vatsa</a>
-                        <a href="#Selkätreenit" class="w3-bar-item w3-button">Selkä</a>
-                        <a href="#Kokokehon" class="w3-bar-item w3-button">Koko keho</a>
+                        <a href="treenit.php#Käsitreenit" class="w3-bar-item w3-button">Kädet</a>
+                        <a href="treenit.php#Jalkatreenit" class="w3-bar-item w3-button">Jalat</a>
+                        <a href="treenit.php#Rintatreenit" class="w3-bar-item w3-button">Rinta</a>
+                        <a href="treenit.php#Vatsatreenit" class="w3-bar-item w3-button">Vatsa</a>
+                        <a href="treenit.php#Selkätreenit" class="w3-bar-item w3-button">Selkä</a>
+                        <a href="treenit.php#Kokokehon" class="w3-bar-item w3-button">Koko keho</a>
                     </div>
                 </div>
                 <a href="yhteystiedot.php" class="w3-bar-item w3-button w3-hide-small w3-hover-white">Yhteystiedot</a>
