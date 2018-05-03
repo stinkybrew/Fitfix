3aae6c5f10a634f5054640e9513d53858c7da8d5
Jkinkku <38202968+Jkinkku@users.noreply.github.com>
2018-05-03 19:50:22 +0300

    Update register2.php
    search-nappi pois

--- register2.php
+++ register2.php
@@ -32,12 +32,12 @@ else {
             <div class="w3-bar w3-theme-d2 w3-left-align">
                 <a class="w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-hover-white w3-theme-d2" href="javascript:void(0);" onclick="openNav()"><i class="fa fa-bars"></i></a>
                 <a href="main.php" class="w3-bar-item w3-button w3-teal">FIXFIT</a>
-                <a href="#" class="w3-bar-item w3-button w3-hide-small w3-right w3-hover-teal" title="Search"><i class="fa fa-search"></i></a>
+              
             </div>
 
             <!-- Navbar on small screens -->
             <div id="navDemo" class="w3-bar-block w3-theme-d2 w3-hide w3-hide-large w3-hide-medium">
-                <a href="#" class="w3-bar-item w3-button">Search</a>
+                
             </div>
         </div>
 
