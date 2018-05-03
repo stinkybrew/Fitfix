40ac48f6a02971bfd03bfcdc35eae7d5de073590
Jkinkku <38202968+Jkinkku@users.noreply.github.com>
2018-05-03 19:52:24 +0300

    Update profile.php
    Search pois ja user-logo profiiliin

--- profile.php
+++ profile.php
@@ -56,7 +56,7 @@ if (isset($_SESSION['first2'])) {
                         $fields = fopen("logout.txt", "r") or die("Unable to open file!");
                         echo fread($fields,filesize("logout.txt"));
                         fclose($fields);
-                        echo '<a href="profile.php" title="Profiili" class="w3-bar-item2 w3-button w3-teal">' . $_SESSION['first'] . '</a>';
+                        echo '<a href="profile.php" title="Profiili" class="w3-bar-item2 w3-button w3-teal"><i class="fa fa-user-circle-o fa-fw w3-margin-right w3-text-theme w3-large"></i>' . $_SESSION['first'] . '</a>';
                     }
 
                     // LOGOUT function !
@@ -86,11 +86,7 @@ if (isset($_SESSION['first2'])) {
                         header("Location:main.php");
                     }
                     ?>
-                </div>    
-                <div>
-                    <a href="#" class="w3-bar-item w3-button w3-hide-small w3-right w3-hover-teal" title="Search"><i class="fa fa-search"></i>
-                    </a>
-                </div>
+                </div>     
             </div>
 
 
