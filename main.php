ab054352e8af4ba438d7e59444e7acbc6d70ffdd
Wille Tuovinen <wille.tuovinen@metropolia.fi>
2018-05-04 10:40:23 +0300

    ..
    ...

--- main.php
+++ main.php
@@ -327,7 +327,7 @@ fclose($testia);
             <a class="w3-button w3-large w3-teal" href="https://www.instagram.com/fixfittreenit/" title="instagram"><i class="fa fa-instagram"></i></a>
             <p>Powered by <a href="main.php" target="_blank">&copy;Team FixFit</a></p>
 
-            <div style="position:relative;bottom:100px;z-index:1;" class="w3-tooltip w3-right">
+            <div style="position:relative;bottom:100px;z-index:1;" class="posfix w3-tooltip w3-right">
                 <span class="w3-text w3-padding w3-teal w3-hide-small">Go To Top</span>   
                 <a class="w3-button w3-theme" href="#myPage"><span class="w3-xlarge">
                     <i class="fa fa-chevron-circle-up"></i></span></a>
