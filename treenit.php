f3ab3654db55af3bcc91b8080cc5c717978f96ec
stinkybrew <wille.tuovinen@metropolia.fi>
2018-05-05 14:05:28 +0300

    koti iconia ja "main2" poisto, koska turha

--- treenit.php
+++ treenit.php
@@ -55,7 +55,7 @@ elseif(!empty($_SESSION['email'])){
         <div class="w3-top">
             <div class="w3-bar w3-theme-d2 w3-left-align">
                 <a class="w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-hover-white w3-theme-d2" href="javascript:void(0);" onclick="openNav()"><i class="fa fa-bars"></i></a>
-                <a href="main.php" class="w3-bar-item w3-button w3-teal">FIXFIT</a>
+                <a href="main.php" class="w3-bar-item w3-button w3-teal"><i class="fa fa-home w3-margin-right"></i>FIXFIT</a>
                 <?php
                 // Open config.ini file, that contains login-info for DB.
                 $config = parse_ini_file("../../config.ini");
