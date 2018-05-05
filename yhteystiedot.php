ef903f0ca17060da2fb285f975ca10443481b663
stinkybrew <wille.tuovinen@metropolia.fi>
2018-05-05 13:18:04 +0300

    ...
    ..

--- yhteystiedot.php
+++ yhteystiedot.php
@@ -133,7 +133,7 @@
                     <div class="w3-modal-content w3-card-4 w3-animate-top">
                         <header class="w3-container w3-teal w3-display-container"> 
                             <span onclick="document.getElementById('id02').style.display='none'" class="w3-button w3-teal w3-display-topright"><i class="fa fa-remove"></i></span>
-                            <h5>Krjaudu tai luo tunnukset <i class="fa fa-smile-o"></i></h5>
+                            <h5>Krjaudu tai luo tunnukset <i class="fa fa-key"></i></h5>
                         </header>
                         <div class="w3-container" style="margin:3%">
                             <div style="background-color:fff" class="w3-hide-medium">
