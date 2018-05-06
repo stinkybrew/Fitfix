f44c03860413f6fa1b9d4f463ca2827248a79b3c
Jkinkku <38202968+Jkinkku@users.noreply.github.com>
2018-05-06 12:27:09 +0300

    Update main.php
    plussa pois

--- main.php
+++ main.php
@@ -23,18 +23,6 @@ fclose($testia);
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <body id="myPage" class="backgroundimg">
 
-        <!-- Sidebar on click -->
-        <nav class="w3-sidebar w3-bar-block w3-white w3-card w3-animate-left w3-xxlarge" style="display:none;z-index:2" id="mySidebar">
-            <a href="javascript:void(0)" onclick="w3_close()" class="w3-bar-item w3-button w3-display-topright w3-text-teal">Sulje
-                <i class="fa fa-remove"></i>
-            </a>
-            <a href="#" class="w3-bar-item w3-button">Link 1</a>
-            <a href="#" class="w3-bar-item w3-button">Link 2</a>
-            <a href="#" class="w3-bar-item w3-button">Link 3</a>
-            <a href="#" class="w3-bar-item w3-button">Link 4</a>
-            <a href="#" class="w3-bar-item w3-button">Link 5</a>
-        </nav>
-
         <!-- Navbar -->
         <div class="w3-top">
             <div class="w3-bar w3-theme-d2 w3-left-align">
@@ -207,11 +195,7 @@ fclose($testia);
                 </footer>
             </div>
         </div>
-        <!-- Container -->
-        <div class="w3-container" style="position:relative">
-            <a onclick="w3_open()" class="w3-button w3-xlarge w3-circle w3-teal"
-               style="position:absolute;top:-28px;right:24px">+</a>
-        </div>
+        
         <!-- Pikatreenit Row -->
         <hr class="hr">
         <div class="w3-container w3-padding-small w3-center">
