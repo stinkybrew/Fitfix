9c08234dd11d6de89bfc385ab4329c3e4e27d4f6
stinkybrew <wille.tuovinen@metropolia.fi>
2020-02-07 21:29:26 +0200

    ...
    ....

--- treenit.php
+++ treenit.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html lang="fi">
     <head>
-        <title>W3.CSS Template</title>
+        <title>FIXFIT</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="w3-update.css">
